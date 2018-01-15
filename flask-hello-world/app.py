e860e54767224557163c0cbef2e80c2f209ed6d0
Cameryn Rhosyn <camerynrhosyn@gmail.com>
2018-01-15 15:09:17 -0500

    First flask application.

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -9,5 +9,29 @@ def hello_world():
     return("Hello, world!")
 
 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return(search_query)
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    return(value, "Next is:", value + 1, "Correct")
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    return(value)
+    return("Next is:")
+    return(value + 1)
+    return("Correct")
+
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    return(value)
+    return("Correct")
+
+
 if __name__ == "__main__":
     app.run()
